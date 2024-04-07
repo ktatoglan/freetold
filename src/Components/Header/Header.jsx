4e8672bc492c80d3f9f104d1df4dc6ce50c8099d
ktatoglan <k.tatoglan@gmail.com>
2024-04-07 19:04:19 +0300

    working modal added
    modal form will be added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import Logo from "../../assets/img/logo-white.svg";
 import User from "../../assets/img/user.png";
 import Modal from "./Modal";
 
 const Header = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  const [openModal, setOpenModal] = useState(false);
 
   const handleLogin = () => {
     // Handle login logic here
@@ -24,45 +16,50 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
-      <div className="logo">
-        <img src={Logo} alt="Freetold"></img>
-      </div>
-      <nav className="menu">
-        <ul>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Properties</a>
-          </li>
-          <li>
-            <a href="#">Contact Us</a>
-          </li>
-          <li>
-            <a href="#" className="log-in">
-              <img src={User} alt="user"></img>Log In
-            </a>
-          </li>
-          <li>
-            <button className="sign-up">Sign Up</button>
-          </li>
-          <li>
-            <button className="write-a-review" onClick={openModal}>
-              Write a review
-            </button>
-            <Modal isOpen={isModalOpen} closeModal={closeModal}>
-              <p>This is the content of the modal.</p>
-            </Modal>
-          </li>
-        </ul>
-      </nav>
-      <button className="hamburger-menu">
-        <span className="line"></span>
-        <span className="line"></span>
-        <span className="line"></span>
-      </button>
-    </header>
+    <>
+      <header className="header">
+        <div className="logo">
+          <img src={Logo} alt="Freetold"></img>
+        </div>
+        <nav className="menu">
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Properties</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+            <li>
+              <a href="#" className="log-in">
+                <img src={User} alt="user"></img>Log In
+              </a>
+            </li>
+            <li>
+              <button className="sign-up">Sign Up</button>
+            </li>
+            <li>
+              <button
+                className="write-a-review"
+                onClick={() => {
+                  setOpenModal(true);
+                }}
+              >
+                Write a review
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <button className="hamburger-menu">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </button>
+      </header>
+      {openModal && <Modal closeModal={setOpenModal} />}
+    </>
   );
 };
 
--- src/Components/Header/Modal.jsx
+++ src/Components/Header/Modal.jsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "../../Style/Modal.css";
 
-const Modal = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Modal = ({ closeModal }) => {
+  const modalRef = useRef();
 
-  const openModal = () => {
-    setIsOpen(true);
-  };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal();
+      }
+    };
 
-  const closeModal = () => {
-    setIsOpen(false);
-  };
+    const handleEscapeKey = (event) => {
+      if (event.key === "Escape") {
+        closeModal();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleEscapeKey);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [closeModal]);
 
   return (
-    <div className={`modal ${isOpen ? "open" : ""}`}>
-      <div className="modal-content">
-        <span className="close" onClick={closeModal}>
-          &times;
-        </span>
-        {children}
+    <div className="modalBackground">
+      <div className="modalContainer" ref={modalRef}>
+        <div className="title">
+          <h3>Let's start with the address</h3>
+        </div>
+        <div className="form">Modal Write a Review Form</div>
       </div>
     </div>
   );
