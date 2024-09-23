46adc98885b3ef37e464b11f5ea958855e03bf12
ktatoglan <k.tatoglan@gmail.com>
2024-09-23 15:39:10 +0300

    desktop done

--- src/Components/Blogs/BlogContent.jsx
+++ src/Components/Blogs/BlogContent.jsx
@@ -1,40 +1,62 @@
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const BlogContent = ({ slug }) => {
-  const [blog, setBlog] = useState({});
-  useEffect(() => {
-    axios.get(`https://blog.freetold.com/wp-json/custom/v1/get-single-post/${slug}`).then((response) => {
-      // console.log(response.data);
-      setBlog(response.data);
-    });
-  }, [slug]);
-  console.log(blog);
-  return (
-    <>
-      <div className="blog-single-main">
-        <div className="blog-single-header">
-          <div className="blog-cat">
-            <p>{blog.categories_data[0].name}</p>
-          </div>
-          <div className="blog-date">
-            {new Date(blog.date_gmt).toLocaleDateString("en-US", {
-              month: "long",
-              day: "numeric",
-              year: "numeric",
-            })}
-          </div>
-        </div>
-        <h1 className="blog-title">
-          {blog.title.rendered}
-        </h1>
+  const [blog, setBlog] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-        <p
-          className="blog-content"
-          dangerouslySetInnerHTML={{ __html: blog.content.rendered }}
-        ></p>
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(
+        `https://blog.freetold.com/wp-json/custom/v1/get-single-post/${slug}`
+      )
+      .then((response) => {
+        setBlog(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Veri çekme hatası:", error);
+        setLoading(false);
+      });
+  }, [slug]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!blog) {
+    return <p>Blog bulunamadı.</p>;
+  }
+
+  // Verileri kontrol ederek render edin
+  return (
+    <div className="blog-single-main">
+      <div className="blog-single-header">
+        <div className="blog-cat">
+          <p>{blog.categories_data && blog.categories_data[0]?.name}</p>
+        </div>
+        <div className="blog-date">
+          {new Date(blog.date_gmt).toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          })}
+        </div>
       </div>
-    </>
+      <h1 className="blog-title">{blog.title?.rendered}</h1>
+      <div className="blog-tags">
+        {blog.tags_data.map((tag) => (
+          <span key={tag.id} className="tag">
+            #{tag.name}
+          </span>
+        ))}
+      </div>
+      <p
+        className="blog-content"
+        dangerouslySetInnerHTML={{ __html: blog.content?.rendered }}
+      ></p>
+    </div>
   );
 };
 
