375ea05daafda451ec88b90714dff36db8fc6c21
zeldox258 <hsynmrgry@gmail.com>
2024-11-15 00:34:12 -0800

    wrong review id redirect

--- src/Components/ReviewProfile/ReviewProfile.jsx
+++ src/Components/ReviewProfile/ReviewProfile.jsx
@@ -14,7 +14,7 @@ function ReviewProfile({reviewID}) {
         </div>
       </div>
     );
- 
+
 
 }
 
--- src/Pages/ReviewProfile.jsx
+++ src/Pages/ReviewProfile.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useParams } from "react-router-dom";
 import ReviewProfile from "../Components/ReviewProfile/ReviewProfile";
 import "../Style/ReviewProfile.css";
+import "axios";
+import { toast } from "react-toastify";
+import  { useAppProvider }  from "../Contexts/AppContext";
+import axios from "axios";
 
 function ReviewProfilePage() {
   const { reviewID } = useParams();
+  const {serverUrl} = useAppProvider();
+  console.log("reviewID" , reviewID);
+  useEffect(() => {
+    axios.get(`${serverUrl}/review/getReview/${reviewID}`)
+    .then((response) => {
+
+    })
+    .catch((error) => {
+      console.log(error);
+      toast.error("Error fetching review, Wrong review ID");
+      //timeout and redirect to home page
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 3000);
+    });
+  }, []);
 
   return (
     <>
