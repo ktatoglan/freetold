023628d50900513f445bc8313eb90540a0d26056
ktatoglan <k.tatoglan@gmail.com>
2024-11-16 15:39:13 +0300

    adding static pages structure

---
+++ src/Pages/StaticPage.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import '../Style/StaticPage.css'; 
+
+const Page = ({ match }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const {slug} = useParams(); // URL'den slug parametresini alıyoruz
+
+  useEffect(() => {
+    // API çağrısı
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://blog.freetold.com/wp-json/custom/v1/get-page/${slug}`);
+        if (response.status === 200) {
+          setData(response.data);
+        } else {
+          throw new Error('Sayfa bulunamadı');
+        }
+      } catch (err) {
+        setError(err.message || 'Bir hata oluştu');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [slug]);
+
+  if (loading) return <div className="page">Loading...</div>;
+
+  if (error) {
+    return (
+      <div className="page">
+        <div className="error-message">
+          <h1>Sayfa Bulunamadı</h1>
+          <p>Aradığınız sayfa mevcut değil ya da URL yanlış olabilir.</p>
+        </div>
+      </div>
+    );
+  }
+
+  const { title, content, featuredmedia } = data;
+
+  return (
+    <div className="page">
+      {/* Başlık Bölümü */}
+      <div
+        className="hero-section"
+        style={{ backgroundImage: `url(${featuredmedia})` }}
+      >
+        <div className="overlay">
+          <h1 className="hero-title">{title}</h1>
+        </div>
+      </div>
+
+      {/* İçerik Bölümü */}
+      <div className="content-section">
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
