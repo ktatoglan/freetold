ee8848f36e3b81220cebf90c23cddef63cd4f222
zeldox258 <hsynmrgry@gmail.com>
2024-11-20 20:39:46 -0800

    search address issue solved

--- src/Components/Header/Modal.jsx
+++ src/Components/Header/Modal.jsx
@@ -18,6 +18,10 @@ const Modal = ({ closeModal }) => {
     setPostCode
   } = useAppProvider();
   const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchByPostcode, setSearchByPostcode] = useState(true);
+  const [searchFullResults, setSearchFullResults] = useState([]);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -41,6 +45,67 @@ const Modal = ({ closeModal }) => {
     };
   }, [closeModal]);
 
+
+
+
+  const token =
+    "ZG9ndWNhbmJhc2tpbkBnbWFpbC5jb206NzljMDc5YjllOTNmMGQ3MWQ3MjIyY2MwYjAyNWM1NDI2NjEwMjg3OA==";
+  const headers = {
+    Accept: "text/csv",
+    Authorization: `Basic ${token}`,
+  };
+  const baseUrl = "https://epc.opendatacommunities.org/api/v1/domestic/search";
+
+  async function searchProperties(query) {
+    if (!query.trim()) {
+      setSearchResults([]);
+      return;
+    }
+    const queryParams = searchByPostcode ? { "postcode": query } : { "address": query };
+    const encodedParams = new URLSearchParams(queryParams).toString();
+    const fullUrl = `${baseUrl}?${encodedParams}`;
+
+    await fetch(fullUrl, {
+      method: "GET",
+      headers: headers,
+    })
+      .then(async (response) => {
+        const responseText = await response.text();
+        const formattedData = csvToKeyValueArray(responseText);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        //console.log(formattedData);
+        setSearchFullResults(formattedData);
+        setSearchResults(formattedData.map(createAddressString));
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+        setSearchResults([]);
+      });
+  }
+
+  function csvToKeyValueArray(csv) {
+    const lines = csv.split("\n");
+    const headers = lines[0].split(",");
+    return lines.slice(1).map((line) => {
+      const values = line.split(",");
+      return headers.reduce((obj, header, index) => {
+        obj[header] = values[index];
+        return obj;
+      }, {});
+    });
+  }
+  function createAddressString(data) {
+    const addressParts = [
+      data["address1"],
+      data["address2"],
+      data["address3"],
+      data["posttown"],
+      data["postcode"],
+    ].filter((part) => part && part.trim() !== "");
+    return addressParts.join(", ");
+  }
   return (
     <div className="modalBackground">
       <div className="modalContainer" ref={modalRef}>
@@ -49,23 +114,57 @@ const Modal = ({ closeModal }) => {
         </div>
         <div className="form">
           <div className="row search-bar">
+            <label>
+              <input
+                type="checkbox"
+                checked={searchByPostcode}
+                onChange={() => setSearchByPostcode(!searchByPostcode)}
+              />
+              Search by postcode
+            </label>
             <input
               type="text"
               className="text-input search-input"
               placeholder="Enter address starting from postcode"
+              onChange={(e) => {
+                setSearchText(e.target.value);
+                searchProperties(e.target.value);
+              }}
             />
+            {searchText.trim() && searchResults.length > 0 && (
+                <ul className="search-results">
+                  {searchResults.map((result, index) => (
+                    <li
+                      key={index}
+                      onClick={() =>{
+                        setTownCity(searchFullResults[index].county);
+                        setCountry("United Kingdom");
+                        setPostCode(searchFullResults[index].postcode);
+                        setAddressLine1(searchFullResults[index]['address1'] + " " + searchFullResults[index]['address2'] + " " + searchFullResults[index]['address3']);
+                        setSearchResults([]);
+                        setSearchText("");
+                      }}
+                    >
+                      {result}
+                    </li>
+                  ))}
+                </ul>
+              )}
           </div>
           <div className="row">
             <div className="col">
-              <p htmlFor="">Address line 1</p>
+              <p htmlFor="">Address line</p>
               <input
                 type="text"
                 className="text-input"
                 placeholder="Street"
                 value={addressLine1}
                 onChange={(e) => setAddressLine1(e.target.value)}
+                readOnly
               />
             </div>
+            {/*
+
             <div className="col">
               <p htmlFor="">Address line 2 (Optional)</p>
               <input
@@ -76,6 +175,8 @@ const Modal = ({ closeModal }) => {
                 onChange={(e) => setAddressLine2(e.target.value)}
               />
             </div>
+
+            */}
           </div>
           <div className="row">
             <div className="col">
@@ -85,6 +186,7 @@ const Modal = ({ closeModal }) => {
                 className="text-input"
                 value={townCity}
                 onChange={(e) => setTownCity(e.target.value)}
+                readOnly
               />
             </div>
             <div className="col">
@@ -94,6 +196,7 @@ const Modal = ({ closeModal }) => {
                 className="text-input"
                 value={country}
                 onChange={(e) => setCountry(e.target.value)}
+                readOnly
               />
             </div>
             <div className="col">
@@ -103,6 +206,7 @@ const Modal = ({ closeModal }) => {
                 className="text-input"
                 value={postCode}
                 onChange={(e) => setPostCode(e.target.value)}
+                readOnly
               />
             </div>
           </div>
