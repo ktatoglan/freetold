b0288828e79bf55d626125d5cec30974c77c6422
ktatoglan <k.tatoglan@gmail.com>
2024-11-24 02:34:07 +0300

    Change the subscribe box in the footer

--- src/Components/HomePage/ContactUs.jsx
+++ src/Components/HomePage/ContactUs.jsx
@@ -22,39 +22,41 @@ export const ContactUs = () => {
         toast.error("Error subscribing to newsletter");
       });
 
-      setEmail("");
+    setEmail("");
   };
   return (
     <section className="contact-us-temp">
       <div className="container">
         <div className="contact-content">
-          <p className="heading">Get in touch with us</p>
           <div className="contact-img">
             <img src={contactImg} alt="" />
           </div>
-          <p className="subheading">
-            Receive an occasional newsletter with our last posts and pro tips
-            about accomodation
-          </p>
-          <form onSubmit={handleSubmit} className="form">
-            <div className="formGroup">
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="input"
-                placeholder="Leave us your email address"
-              />
-            </div>
-            <button type="submit" className="button">
-              Subscribe
-            </button>
-          </form>
-          <p className="warn">
-            By subscribing you agree to Terms of Use, our Privacy Policy and our
-            Information collection notice
-          </p>
+          <div className="contact-form-container">
+            <p className="heading">Get in touch with us</p>
+            <p className="subheading">
+              Receive an occasional newsletter with our last posts and pro tips
+              about accomodation
+            </p>
+            <form onSubmit={handleSubmit} className="form">
+              <div className="formGroup">
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="input"
+                  placeholder="Leave us your email address"
+                />
+              </div>
+              <button type="submit" className="button">
+                Subscribe
+              </button>
+            </form>
+            <p className="warn">
+              By subscribing you agree to Terms of Use, our Privacy Policy and
+              our Information collection notice
+            </p>
+          </div>
         </div>
       </div>
     </section>
