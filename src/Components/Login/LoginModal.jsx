d3c17c899ff3ddd20182adc7573c01d0e3091e4d
zeldox258 <hsynmrgry@gmail.com>
2024-11-24 20:40:36 -0800

    google login update

--- src/Components/Login/LoginModal.jsx
+++ src/Components/Login/LoginModal.jsx
@@ -58,7 +58,7 @@ const LoginModal = ({ closeLoginModal }) => {
         <div className="login-buttons row">
           <div className="col">
             <div className="login-google-div">
-              <button className="google-login">
+              <button className="google-login" onClick={()=>{toast.error("Coming soon!")}}>
                 <div className="logo">
                   <svg
                     width="24"
