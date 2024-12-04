eca6b14f1cbe128ae1017c0734e95c2919c2dc96
ktatoglan <k.tatoglan@gmail.com>
2024-12-04 13:42:55 +0300

    revert registration rules

--- src/Components/Register/RegisterModal.jsx
+++ src/Components/Register/RegisterModal.jsx
@@ -8,70 +8,38 @@ const RegisterModal = ({ closeRegisterModal }) => {
   const RegisterModalRef = useRef();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
+  const [staying, setStaying] = useState("");
+  const [postcode, setPostcode] = useState("");
   const [password, setPassword] = useState("");
-  const { serverUrl, setUserId } = useAppProvider();
+  const { serverUrl, userId, setUserId } = useAppProvider();
+  // get referer_id from local storage
   const referer_id = localStorage.getItem("referer_id");
 
-  const resetForm = () => {
-    setEmail("");
-    setName("");
-    setPassword("");
-  };
-
-  const validateForm = () => {
-    // No empty field is allowed
-    if (!email || !name || !password) {
-      toast.error("All fields must be filled.");
-      return false;
-    }
-
-    // Email should be in email format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      toast.error("Please enter a valid email address.");
-      return false;
-    }
-
-    // Password should include upper, lower case, number and a special character
-    const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if (!passwordRegex.test(password)) {
-      toast.error(
-        "Password must include an uppercase letter, a lowercase letter, a number, and a special character."
-      );
-      return false;
-    }
-
-    return true;
-  };
-
-  const handleRegister = async () => {
-    if (!validateForm()) return;
-
-    try {
-      const response = await axios.post(`${serverUrl}/register`, {
+  const handleRegister = () => {
+    axios
+      .post(`${serverUrl}/register`, {
         email,
         name,
+        postcode,
         password,
-        referer_id,
-      });
-      toast.success(response.data.message);
-      setUserId(response.data.userId);
-      resetForm();
-      closeRegisterModal();
+        referer_id
+      })
+      .then((response) => {
+        toast.success(response.data.message);
+        setUserId(response.data.userId);
+        closeRegisterModal();
+        // remove referer_id from local storage
+        localStorage.removeItem("referer_id");
+        //remove referer_id from url
+        const url = window.location.href;
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.delete("referer_id");
 
-      // Referer ID temizliği
-      localStorage.removeItem("referer_id");
-      const urlParams = new URLSearchParams(window.location.search);
-      urlParams.delete("referer_id");
-      window.history.replaceState({}, "", `${window.location.pathname}`);
-    } catch (error) {
-      console.error("Error:", error);
-      const errorMessage =
-        error.response?.data?.message ||
-        "An error occurred during registration.";
-      toast.error(errorMessage);
-    }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error(error);
+      });
   };
 
   useEffect(() => {
@@ -117,23 +85,25 @@ const RegisterModal = ({ closeRegisterModal }) => {
         </div>
         <div className="login-buttons row">
           <div className="col">
-            <button className="google-login">
-              <div className="logo">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M21.3503 11.1H12.1803V13.83H18.6903C18.3603 17.64 15.1903 19.27 12.1903 19.27C8.36027 19.27 5.00027 16.25 5.00027 12C5.00027 7.9 8.20027 4.73 12.2003 4.73C15.2903 4.73 17.1003 6.7 17.1003 6.7L19.0003 4.72C19.0003 4.72 16.5603 2 12.1003 2C6.42027 2 2.03027 6.8 2.03027 12C2.03027 17.05 6.16027 22 12.2503 22C17.6003 22 21.5003 18.33 21.5003 12.91C21.5003 11.76 21.3503 11.1 21.3503 11.1Z"
-                    fill="#0A2446"
-                  />
-                </svg>
-              </div>
-              <p>Continue with Google</p>
-            </button>
+            <div className="login-google-div">
+              <button className="google-login">
+                <div className="logo">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M21.3503 11.1H12.1803V13.83H18.6903C18.3603 17.64 15.1903 19.27 12.1903 19.27C8.36027 19.27 5.00027 16.25 5.00027 12C5.00027 7.9 8.20027 4.73 12.2003 4.73C15.2903 4.73 17.1003 6.7 17.1003 6.7L19.0003 4.72C19.0003 4.72 16.5603 2 12.1003 2C6.42027 2 2.03027 6.8 2.03027 12C2.03027 17.05 6.16027 22 12.2503 22C17.6003 22 21.5003 18.33 21.5003 12.91C21.5003 11.76 21.3503 11.1 21.3503 11.1Z"
+                      fill="#0A2446"
+                    />
+                  </svg>
+                </div>
+                <p>Continue with Google</p>
+              </button>
+            </div>
           </div>
         </div>
         <div className="or">
@@ -143,7 +113,7 @@ const RegisterModal = ({ closeRegisterModal }) => {
         <div className="form">
           <div className="row">
             <div className="col">
-              <p>Email address</p>
+              <p htmlFor="">Email address</p>
               <input
                 type="email"
                 className="text-input"
@@ -155,7 +125,7 @@ const RegisterModal = ({ closeRegisterModal }) => {
           </div>
           <div className="row">
             <div className="col">
-              <p>What should we call you?</p>
+              <p htmlFor="">What should we call you?</p>
               <input
                 type="text"
                 className="text-input"
@@ -165,9 +135,21 @@ const RegisterModal = ({ closeRegisterModal }) => {
               />
             </div>
           </div>
+          {/* <div className="row">
+            <div className="col">
+              <p htmlFor="">Your Current Postcode</p>
+              <input
+                type="text"
+                className="text-input"
+                placeholder="Address"
+                value={postcode}
+                onChange={(e) => setPostcode(e.target.value)}
+              />
+            </div>
+          </div> */}
           <div className="row">
             <div className="col">
-              <p>Password</p>
+              <p htmlFor="">Password</p>
               <input
                 type="password"
                 className="text-input"
@@ -176,22 +158,39 @@ const RegisterModal = ({ closeRegisterModal }) => {
                 onChange={(e) => setPassword(e.target.value)}
               />
               <p className="pw-info">
-                Should include uppercase letter, lowercase letter, number, and a
-                special character.
+                Should include uppercase letter, number and 8 characters or more
               </p>
             </div>
           </div>
+          {/*
+            <div className="row m-0">
+            <div className="col">
+              <div className="toggle-group">
+                <input type="checkbox" id="switch" />
+                <label for="switch">Toggle</label>
+                <p>Stay signed in</p>
+              </div>
+            </div>
+          </div>
+          */}
           <div className="row">
             <div className="col">
               <p className="register-agree">
                 By proceeding, you agree to our Terms of Use and confirm you
-                have read our Privacy and Cookie Statement.
+                have read our Privacy and Cookie Statement. This site is
+                protected by reCAPTCHA and the Google Privacy Policy and Terms
+                of Service apply.
               </p>
             </div>
           </div>
           <div className="row">
             <div className="col">
-              <button className="write-a-review" onClick={handleRegister}>
+              <button
+                className="write-a-review"
+                onClick={async () => {
+                  await handleRegister();
+                }}
+              >
                 Create an account
               </button>
             </div>
