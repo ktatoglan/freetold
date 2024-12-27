19f4e946eea095dfa7bf689627dd82023057e342
zeldox258 <hsynmrgry@gmail.com>
2024-12-27 15:30:51 -0800

    add meta description tag for SEO in HomePage component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import HomePageText from '../Components/HomePage/HomePageText';
 import HomePageTextWelcome from '../Components/HomePage/HomePageTextWelcome';
 import SearchBar from '../Components/HomePage/SearchBar';
@@ -10,7 +10,12 @@ import { ContactUs } from '../Components/HomePage/ContactUs';
 import { LatestReviews } from '../Components/HomePage/LatestReviews';
 import BlogSection from '../Components/HomePage/BlogSection';
 const HomePage = () => {
-  // Add your component logic here
+  useEffect(() => {
+    //add meta description tag for SEO
+    const meta = document.createElement('meta');
+    meta.name = 'description';
+    meta.content = 'Freetold is your trusted guide to the UK housing market. Discover real tenant reviews, insights into neighbourhoods, living costs, and utility bills. Learn about landlords, property conditions, and more to make confident renting or buying decisions. Join the community transforming how we find and share property information.';
+  },[]);
 
   return (
     <div>
