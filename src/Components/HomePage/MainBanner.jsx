2185e500e8ebd06afccf015dc9895c505b68efc8
ktatoglan <k.tatoglan@gmail.com>
2025-01-09 19:18:04 +0300

    Preload Largest Contentful Paint image

--- src/Components/HomePage/MainBanner.jsx
+++ src/Components/HomePage/MainBanner.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "./SearchBar";
+import desktopImage from "../../assets/img/main-banner.svg";
+import mobileImage from "../../assets/img/mbl-main-banner-new.svg";
 
 export const MainBanner = () => {
+
+  useEffect(() => {
+    const preloadLinkDesktop = document.createElement("link");
+    preloadLinkDesktop.rel = "preload";
+    preloadLinkDesktop.as = "image";
+    preloadLinkDesktop.href = desktopImage;
+    document.head.appendChild(preloadLinkDesktop);
+
+    const preloadLinkMobile = document.createElement("link");
+    preloadLinkMobile.rel = "preload";
+    preloadLinkMobile.as = "image";
+    preloadLinkMobile.href = mobileImage;
+    document.head.appendChild(preloadLinkMobile);
+  }, []);
+
   return (
     <section className="main-banner">
-      <div className="container ">
+      <picture>
+        <source media="(max-width: 560px)" srcSet={mobileImage} />
+        <img
+          src={desktopImage}
+          alt="Main Banner"
+          className="main-banner-image"
+        />
+      </picture>
+      <div className="container">
         <div className="homepage-text">
           <p className="badge">Unlock Property Insights</p>
           <h1 className="title">
